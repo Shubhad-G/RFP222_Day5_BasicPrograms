45c06e23733f6c172af263b42b913fee73a45691
Shubhad Gonal <shubhadgonal19@gmail.com>
2022-11-16 23:07:06 +0530

    even or add program

---
+++ BasicCorePrograms/EvenOdd.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class EvenOdd
+    {
+        public static void evenOdd(int n)
+        {
+            if (n % 2 == 0)
+                Console.WriteLine("{0} is even number",n);
+            else
+                Console.WriteLine("{0} is odd number",n);
+        }
+    }
+}
