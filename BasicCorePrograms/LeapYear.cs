02954ca9d7d302b3e5ff996b846604ced9f9fbf0
Shubhad Gonal <shubhadgonal19@gmail.com>
2022-11-16 08:57:21 +0530

    implemented Leap Year program

---
+++ BasicCorePrograms/LeapYear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class LeapYear
+    {
+        public static void leapYear(int n)
+        {
+            if (n > 999 && n < 10000)
+            {
+                if (n % 4 == 0)
+                    Console.WriteLine("{0} year is Leap year", n);
+                else if (n % 400 == 0)
+                    Console.WriteLine("{0} year is Leap year", n);
+                else if (n % 100 == 0)
+                    Console.WriteLine("{0} year is not a Leap year", n);
+                else
+                    Console.WriteLine("{0} year is not a Leap year", n);
+            }
+            else
+                Console.WriteLine("incorrect input,enter 4 digit number for year");
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -14,6 +14,9 @@ namespace BasicCorePrograms
             Console.WriteLine("enter how many times coin has to be flipped");
             x=Convert.ToInt32(Console.ReadLine());
             FlipCoin.flip(x);
+            Console.WriteLine("enter the year to find whether the given year is leap year or not");
+            int n = Convert.ToInt32(Console.ReadLine());
+            LeapYear.leapYear(n);
             Console.ReadLine();
         }
     }
