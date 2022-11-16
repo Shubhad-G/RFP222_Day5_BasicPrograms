485cac4043a57db57ab2cd7a7e0e8081da138b08
Shubhad Gonal <shubhadgonal19@gmail.com>
2022-11-16 21:55:44 +0530

    Swapping of two numbers

---
+++ BasicCorePrograms/Swap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class Swap
+    {
+        public static void swapTwoNumbers(int a,int b)
+        {
+            int temp;
+            Console.WriteLine("\nbefore swap first number={0}", a);
+            Console.WriteLine("before swap second number={0}", b);
+            temp = a;
+            a=b;
+            b=temp;
+            Console.WriteLine("\nafter swap first number={0}", a);
+            Console.WriteLine("after swap second number={0}", b);
+        }
+    }
+}
